eaf59f331f52badd0efd06b0cc74611a43c83c8a
Tom <64475286+TomSwallaxz@users.noreply.github.com>
2020-10-17 00:11:44 +0300

    Change App number 5

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,4 @@
-import React, { Fragment, useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from "./components/container/NavBar";
-// import Home from "./components/container/Home";
-// import Lecture from "./components/uesrComponents/userComponents";
-// import StudentProfile from "./components/uesrComponents/StudentProfile";
-// import About from "./components/container/About";
-// import Login from "./components/container/LogIn";
-// import Register from "./components/container/Register";
-// import axios from "axios";
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -30,43 +21,13 @@ function App() {
       }).catch((err) => console.log(err));
   }, []);
 
-  // const [user, setUser] = useState({
-  //   token: null,
-  //   user: null,
-  // });
-  // const checkLogin = async () => {
-  //   let token = localStorage.getItem("authToken");
-  //   const tokenRes = await axios.post("/api/users/tokenIsValid", null, {
-  //     headers: { "x-auth-token": token },
-  //   });
-  //   if (tokenRes.data) {
-  //     setUser({
-  //       token: tokenRes.data.token,
-  //       user: tokenRes.data.user,
-  //     });
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   checkLogin();
-  // }, []);
+  
 
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        {/* <Route exact path="/home" component={Home} />
-        <section className="container">
-          <Switch>
-            <Route exact path="/studentProfile" component={StudentProfile} />
-            <Route exact path="/lecture" component={Lecture} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-          </Switch>
-        </section> */}
-      </Fragment>
-    </Router>
+    <div className="App">
+      <h1>hello word tom is update now</h1>
+
+    </div>
   );
 }
 export default App;
