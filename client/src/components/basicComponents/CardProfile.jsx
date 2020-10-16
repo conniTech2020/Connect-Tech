286f9ade8838ed9e8dc9c75ae32189d8956ca6bb
Tom <64475286+TomSwallaxz@users.noreply.github.com>
2020-10-16 23:27:36 +0300

    Change

---
+++ client/src/components/basicComponents/CardProfile.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Avatar from "./Avatar";
+
+function Card(props) {
+  return (
+    <div class="column">
+      <div class="card">
+        <div class="container">
+          <Avatar img={props.img} />
+          <h2>{props.name}</h2>
+          <p class="title">{props.title}</p>
+          <p>{props.someTXT}</p>
+          <p>mike@example.com</p>
+          <p>{props.phone}</p>
+          <p className="Crardicon">{props.email}</p>
+          <p>{props.age}</p>
+          <p>
+            <button class="button">Contact</button>
+          </p>
+        </div>
+      </div>
+      <div className="aboutMi">
+
+      </div>
+      <div className="moreAboutMi">
+
+      </div>
+    </div>
+  );
+}
+
+export default Card;
