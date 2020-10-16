cf2c651746c62ced79e492010202ba004aff992b
Tom <64475286+TomSwallaxz@users.noreply.github.com>
2020-10-16 23:55:22 +0300

    Change App number 3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,14 @@
-import React, { useEffect, useState } from 'react';
 import './App.css';
+import React, { Fragment, useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/container/NavBar";
+import Home from "./components/container/Home";
+import Lecture from "./components/uesrComponents/userComponents";
+import StudentProfile from "./components/uesrComponents/StudentProfile";
+import About from "./components/container/About";
+import Login from "./components/container/LogIn";
+import Register from "./components/container/Register";
+import axios from "axios";
 
 function App() {
   const [users, setUsers] = useState([]);
@@ -21,11 +30,43 @@ function App() {
       }).catch((err) => console.log(err));
   }, []);
 
-  return (
-    <div className="App">
-      <h1>hello word</h1>
+  const [user, setUser] = useState({
+    token: null,
+    user: null,
+  });
+  const checkLogin = async () => {
+    let token = localStorage.getItem("authToken");
+    const tokenRes = await axios.post("/api/users/tokenIsValid", null, {
+      headers: { "x-auth-token": token },
+    });
+    if (tokenRes.data) {
+      setUser({
+        token: tokenRes.data.token,
+        user: tokenRes.data.user,
+      });
+    }
+  };
 
-    </div>
+  useEffect(() => {
+    checkLogin();
+  }, []);
+
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/home" component={Home} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/studentProfile" component={StudentProfile} />
+            <Route exact path="/lecture" component={Lecture} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
   );
 }
 export default App;
