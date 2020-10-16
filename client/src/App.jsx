5200e6c4fe988fbe41a75a42ed7b445974578538
Tom <64475286+TomSwallaxz@users.noreply.github.com>
2020-10-17 00:18:15 +0300

    Change App number 6

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,13 @@
 import React, { useEffect, useState } from 'react';
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Navbar from "./components/container/NavBar";
+// import Home from "./components/container/Home";
+// import Lecture from "./components/uesrComponents/userComponents";
+// import StudentProfile from "./components/uesrComponents/StudentProfile";
+// import About from "./components/container/About";
+import Login from "./components/container/LogIn";
+// import Register from "./components/container/Register";
+// import axios from "axios";
 import './App.css';
 
 function App() {
@@ -26,6 +35,22 @@ function App() {
   return (
     <div className="App">
       <h1>hello word tom is update now</h1>
+      <Login/>
+      {/* <Router> */}
+      {/* <Fragment> */}
+        {/* <Navbar /> */}
+        {/* <Route exact path="/home" component={Home} /> */}
+        {/* <section className="container"> */}
+          {/* <Switch> */}
+            {/* <Route exact path="/studentProfile" component={StudentProfile} /> */}
+            {/* <Route exact path="/lecture" component={Lecture} /> */}
+            {/* <Route exact path="/about" component={About} /> */}
+            {/* <Route exact path="/login" component={Login} /> */}
+            {/* <Route exact path="/register" component={Register} /> */}
+          {/* </Switch> */}
+        {/* </section> */}
+      {/* </Fragment> */}
+    {/* </Router> */}
 
     </div>
   );
