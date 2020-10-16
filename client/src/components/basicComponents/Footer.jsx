492b7303a2accd12b9c235b9a288dd27790009ac
Tom <64475286+TomSwallaxz@users.noreply.github.com>
2020-10-16 23:22:42 +0300

    Change

---
+++ client/src/components/basicComponents/Footer.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+
+export const Footer = () => {
+    return (
+<div>
+
+  <div className="containerFooter ">
+
+    <div  className="row">
+
+      <div  className="iconsFooter">
+        <div className="mb-5 flex-center">
+
+          <a className="iconFooter">
+            <i className="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+          </a>
+          <a className="iconFooter">
+            <i className="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+          </a>
+          <a className="iconFooter">
+            <i className="fab fa-google-plus-g fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+          </a>
+          <a className="iconFooter">
+            <i className="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+          </a>
+          <a className="iconFooter">
+            <i className="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+          </a>
+          <a className="iconFooter">
+            <i className="fab fa-pinterest fa-lg white-text fa-2x"> </i>
+          </a>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+
+
+</div>
+     
+    )
+}
+
+export default Footer;
